b03fcb71e6d6e11c634d649d206dbc5a125597bf
Nicholas Schwartz <nicholasschwartz@Nicholass-MacBook-Air.local>
2024-01-18 10:46:22 -0500

    New matrix creation w/ pointers. Swapped multiplication order. Changed data writing

--- Nick_Work/Schwartz_Project1.cpp
+++ Nick_Work/Schwartz_Project1.cpp
@@ -12,53 +12,60 @@
 
 using namespace std;
 
-/*void multiplymatrix ( int matrixsize, float matrixA[matrixsize][matrixsize], float matrixB[matrixsize][matrixsize],
-float* matrixC[matrixsize][matrixsize] )
+
+// Jared Provided me this code snippet because I was struggling with properly doing double pointer
+// and using malloc
+float** generate_matrix(int size) {
+    float** matrix = (float**)malloc(size * sizeof(int*));
+    for (int i = 0; i < size; i++) {
+        matrix[i] = (float*)malloc(size * sizeof(int));
+    }
+    // Random seed for number generation
+    srand(time(0));
+    // Fill the matrix with random numbers
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            // matrix[i][j] = rand() % 100; // Adjust the range
+            matrix[i][j] =  (float)rand()/(float)(RAND_MAX);
+        }
+    }
+    return matrix;
+}
+// end code provided by Jared
+
+float** multiplymatrix ( int matrixsize, float** matrixA, float** matrixB,
+float** matrixC )
 {
-        for (int i = 0; i < matrixsize; ++i) {
-            for (int j = 0; j < matrixsize; ++j) {
-                *matrixC[i][j] = 0;
-                for (int k = 0; k < matrixsize; ++k) 
-                {
-                    *matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
-                }
+    for (int i = 0; i < matrixsize; ++i) {
+        for (int k = 0; k < matrixsize; ++k)  {
+            for (int j = 0; j < matrixsize; ++j)
+            {
+            matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
             }
         }
-}*/
+    }
+    return matrixC;
+}
 
 
 int main() {
-    int finishsize = 100;
-    float sizes[finishsize];
-    float times[finishsize];
+    int finishsize = 1;
+    ofstream myfile ("data.txt");
+
     for (int i = 0; i < finishsize; ++i) {
-        int matrixsize = i+1;
-        
-        srand(time(0));
-        // fill each matrix with random integers  : A, B
+        int matrixsize = 2000;
 
-        float matrixA[matrixsize][matrixsize];
-        float matrixB[matrixsize][matrixsize];
-        for (int i = 0; i < matrixsize; ++i) {
-            for (int j = 0; j < matrixsize; ++j) {
-                matrixA[i][j] = (rand() % 10) ;
-            }
-        }
-        for (int i = 0; i < matrixsize; ++i) {
-            for (int j = 0; j < matrixsize; ++j) {
-                matrixB[i][j] = (rand() % 10) ;
-            }
-        }
-
+        float** matrixA = generate_matrix(matrixsize);
+        float** matrixB = generate_matrix(matrixsize);
+        float** matrixC = generate_matrix(matrixsize);
 
         // timing is from 
         // https://stackoverflow.com/questions/56138064/how-do-i-convert-stdchronohigh-resolution-type-to-a-float-type
 
         std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
-        float matrixC[matrixsize][matrixsize];
-        //multiplymatrix ( matrixsize, matrixA, matrixB, matrixC );
-        for (int i = 0; i < matrixsize; ++i) {
+        multiplymatrix ( matrixsize, matrixA, matrixB, matrixC );
+        /*for (int i = 0; i < matrixsize; ++i) {
             for (int j = 0; j < matrixsize; ++j) {
                 matrixC[i][j] = 0;
                 for (int k = 0; k < matrixsize; ++k) 
@@ -66,26 +73,21 @@ int main() {
                     matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
                 }
             }
-        }
+        }*/
         std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
         float duration = std::chrono::duration<float>(end - start).count();
 
-        sizes[i] = matrixsize;
-        times[i] = duration;
+        myfile << matrixsize;
+        myfile << ",";
+        myfile << duration;
+        myfile << "\n";
 
     }
-    ofstream myfile ("data.txt");
 
     // below modified from 
     // https://stackoverflow.com/questions/22190048/how-to-write-the-content-of-an-array-into-a-text-file
 
-    for(int i = 0; i < finishsize; i++){
-			myfile << sizes[i];
-			myfile << ",";
-			myfile << times[i];
-			myfile << "\n";
-		}
 	myfile.close();
     
 }
